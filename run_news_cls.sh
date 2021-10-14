af2cfa7f5896eebff06966e7a2465f4b5c104b3d
VitoUnderwood <1240505429@qq.com>
2021-10-14 15:53:44 +0800

    中文新闻基于BERT的分类器
    整体参考bert中的run_classifier.py， 修改TPU estimator到普通的GPU上，
    优化代码结构，将train和eval、predict均跑通，base tensorfolw-gpu 115
    或cpu，模型在dev上的效果存在eval_results.txt中，整个项目结构还需要进
    一步优化

--- run_news_cls.sh
+++ run_news_cls.sh
@@ -1,31 +1,48 @@
-export BERT_BASE_DIR=/Users/vito/PyCharmProjects/nlp_estimator_template/checkpoints/chinese_L-12_H-768_A-12
-export DATA_DIR=/Users/vito/PyCharmProjects/nlp_estimator_template/data/THUCNews
-export OUTPUT_DIR=/Users/vito/PyCharmProjects/nlp_estimator_template/checkpoints/news_cls
-
-python run_news_cls.py \
-  --do_train=true \
-  --do_eval=true \
-  --data_dir=$DATA_DIR/MRPC \
-  --vocab_file=$BERT_BASE_DIR/vocab.txt \
-  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-  --max_seq_length=128 \
-  --train_batch_size=32 \
-  --learning_rate=2e-5 \
-  --num_train_epochs=3.0 \
-  --output_dir=$OUTPUT_DIR
-
-# predict
-#export BERT_BASE_DIR=/Users/vito/PyCharmProjects/nlp_estimator_template/checkpoints/uncased_L-12_H-768_A-12
-#export GLUE_DIR=/Users/vito/PyCharmProjects/nlp_estimator_template/data
-#export TRAINED_CLASSIFIER=/Users/vito/PyCharmProjects/nlp_estimator_template/checkpoints/tmp
+#export BERT_BASE_DIR=checkpoints/chinese_L-12_H-768_A-12
+#export DATA_DIR=data/News
+#export OUTPUT_DIR=checkpoints/news_cls
 #
-#python run_classifier.py \
-#  --task_name=MRPC \
-#  --do_predict=true \
-#  --data_dir=$GLUE_DIR/MRPC \
+#python run_news_cls.py \
+#  --do_train=true \
+#  --do_eval=true \
+#  --data_dir=$DATA_DIR/ \
 #  --vocab_file=$BERT_BASE_DIR/vocab.txt \
 #  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-#  --init_checkpoint=$TRAINED_CLASSIFIER \
+#  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
 #  --max_seq_length=128 \
-#  --output_dir=/tmp/mrpc_output/
+#  --train_batch_size=8 \
+#  --learning_rate=2e-5 \
+#  --num_train_epochs=3.0 \
+#  --output_dir=$OUTPUT_DIR
+
+## eval
+#export BERT_BASE_DIR=checkpoints/chinese_L-12_H-768_A-12
+#export DATA_DIR=data/News
+#export OUTPUT_DIR=checkpoints/news_cls
+#
+#python run_news_cls.py \
+#  --do_eval=true \
+#  --data_dir=$DATA_DIR/ \
+#  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+#  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+#  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+#  --max_seq_length=128 \
+#  --train_batch_size=8 \
+#  --learning_rate=2e-5 \
+#  --num_train_epochs=3.0 \
+#  --output_dir=$OUTPUT_DIR
+
+# predict
+export BERT_BASE_DIR=checkpoints/chinese_L-12_H-768_A-12
+export DATA_DIR=data/News
+export TRAINED_CLASSIFIER=checkpoints/news_cls
+export OUTPUT_DIR=checkpoints/news_cls
+
+python run_news_cls.py \
+  --do_predict=true \
+  --data_dir=$DATA_DIR/ \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$TRAINED_CLASSIFIER \
+  --max_seq_length=128 \
+  --output_dir=$OUTPUT_DIR
